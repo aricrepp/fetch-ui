97509afe24ecfea7657158680dfa05102e7eb191
Aric Repp <repp.a@pg.com>
2025-02-27 14:19:38 -0500

    File routing update

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,32 @@
 "use client";
 
 import { Suspense } from "react";
-import DogMatch from "./dogMatch/page";
-import DogsInput from "./dogInput/page";
-import DogCards from "./dogCards/page";
+import DogMatch from "./_components/dogMatch/page";
+import DogsInput from "./_components/dogInput/page";
+import DogCards from "./_components/dogCards/page";
 import {
   useGetAllDogsQuery,
   useGetDogBreedsQuery,
 } from "@/utils/service/rtkQuery";
+import LoadingCards from "./_components/dogCards/loading";
 
 export default function Dashboard() {
   const { data: breedData } = useGetDogBreedsQuery();
   const { data: searchData } = useGetAllDogsQuery();
+
+  if (!searchData)
+    return (
+      <section
+        id="dog-dashboard"
+        className="flex flex-col justify-start h-dvh items-center bg-gray-200 font-[family-name:var(--font-geist-sans)] py-10 overflow-x-hidden overflow-y-scroll"
+      >
+        <Suspense>
+          <DogsInput dogBreeds={breedData} />
+        </Suspense>
+        <LoadingCards />
+        <DogMatch />
+      </section>
+    );
 
   return (
     <section
@@ -21,7 +36,9 @@ export default function Dashboard() {
       <Suspense>
         <DogsInput dogBreeds={breedData} />
       </Suspense>
-      <DogCards searchData={searchData} />
+      <Suspense>
+        <DogCards searchData={searchData} />
+      </Suspense>
       <DogMatch />
     </section>
   );
